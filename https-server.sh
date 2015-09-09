4de77506ec017a914acdb8980d20b386a59aa20e
Thaddee Tyl <thaddee.tyl@gmail.com>
2015-09-09 10:30:15 +0200

    Make forever start the HTTPS server from the node script

--- https-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "${BASH_SOURCE[0]}")
-export HTTPS=true
-export SHIELDS_ANALYTICS_FILE="$DIR"/analytics-https.json
-node "$DIR"/server.js
--- start.sh
+++ start.sh
@@ -8,10 +8,12 @@ forever start -a \
   -e "$SHIELDS_DIR"/log/err.log \
   --minUptime 1000 --spinSleepTime 1000 \
   server.js
+export HTTPS=true
+export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 forever start -a \
   -l "$SHIELDS_DIR"/log/https-forever.log \
   -o "$SHIELDS_DIR"/log/https-out.log \
   -e "$SHIELDS_DIR"/log/https-err.log \
   --minUptime 1000 --spinSleepTime 1000 \
-  -c bash https-server.sh
+  server.js
 popd
