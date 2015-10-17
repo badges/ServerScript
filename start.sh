c7ea8ed2f82442b8f444db9522f3ff7af9485b31
Thaddee Tyl <thaddee.tyl@gmail.com>
2015-10-17 19:07:37 +0200

    Start the redirect script automatically

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SHIELDS_DIR="$DIR/shields"
-pushd "$SHIELDS_DIR"
 forever start -a \
   -l "$SHIELDS_DIR"/log/forever.log \
   -o "$SHIELDS_DIR"/log/out.log \
   -e "$SHIELDS_DIR"/log/err.log \
   --minUptime 1000 --spinSleepTime 1000 \
-  server.js
+  http-redirect.js
+pushd "$SHIELDS_DIR"
 export HTTPS=true
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 forever start -a \
