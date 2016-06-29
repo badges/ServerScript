868a7f69220e36996443c3b25f7095ea25062045
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-06-29 20:32:09 +0200

    Set correct directory for analytics file

--- start-shields.sh
+++ start-shields.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /home/m/shields/
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SHIELDS_DIR="$DIR/shields"
+cd "$SHIELDS_DIR"
 export HTTPS=true
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 node ./server.js >> log/out 2>&1
