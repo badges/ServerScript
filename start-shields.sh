b838556f4f91cd84c8897cbbac92fd6d0ff71ae4
Paul Melnikow <github@paulmelnikow.com>
2017-11-11 18:19:29 -0500

    Set FRONTEND_REDIRECT_URL
    For badges/shields#1270

--- start-shields.sh
+++ start-shields.sh
@@ -4,5 +4,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SHIELDS_DIR="$DIR/shields"
 cd "$SHIELDS_DIR"
 export HTTPS=true
+export FRONTEND_REDIRECT_URL=https://shields.io/
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 node --max_old_space_size=1380 ./server.js >> log/out 2>&1
