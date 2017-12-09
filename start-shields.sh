38591ee6cc3c18bff0fed72ccadb2acfe4e2915d
Thaddee Tyl <thaddee.tyl@gmail.com>
2017-12-09 18:33:59 +0100

    Disable GitHub token debugging
    As discussed in https://github.com/badges/shields/issues/1314.

--- start-shields.sh
+++ start-shields.sh
@@ -7,4 +7,5 @@ export HTTPS=true
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 export ALLOWED_ORIGIN=http://shields.io,https://shields.io
 export REDIRECT_URI=https://shields.io/
+export GITHUB_DEBUG_ENABLED=false
 node --max_old_space_size=1380 ./server.js >> log/out 2>&1
