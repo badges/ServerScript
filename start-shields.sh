84300f3402e06a0c2eb70b3d782bf38ae9aa9564
Paul Melnikow <email@paulmelnikow.com>
2019-01-06 12:32:43 -0500

    Config update

--- start-shields.sh
+++ start-shields.sh
@@ -5,7 +5,5 @@ SHIELDS_DIR="$DIR/shields"
 cd "$SHIELDS_DIR"
 export HTTPS=true
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
-export ALLOWED_ORIGIN=http://shields.io,https://shields.io
-export REDIRECT_URI=https://shields.io/
-export GITHUB_DEBUG_ENABLED=false
+export NODE_CONFIG_ENV=shields-io-production
 node --max_old_space_size=1380 ./server.js >> log/out 2>&1
