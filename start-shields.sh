21e01098da8cde55d5826f344300cb4f272d67c3
Paul Melnikow <email@paulmelnikow.com>
2019-01-06 12:36:07 -0500

    This is set in main repo

--- start-shields.sh
+++ start-shields.sh
@@ -3,7 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SHIELDS_DIR="$DIR/shields"
 cd "$SHIELDS_DIR"
-export HTTPS=true
 export SHIELDS_ANALYTICS_FILE="$SHIELDS_DIR"/analytics-https.json
 export NODE_CONFIG_ENV=shields-io-production
 node --max_old_space_size=1380 ./server.js >> log/out 2>&1
